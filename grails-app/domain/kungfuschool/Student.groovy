fdc82068367b3e1a13830398e1398de65a331183
brucelau_github <brucelau.email@gmail.com>
2017-03-30 12:50:36 -0400

    add student mobilenumber

--- grails-app/domain/kungfuschool/Student.groovy
+++ grails-app/domain/kungfuschool/Student.groovy
@@ -2,9 +2,14 @@ package kungfuschool
 
 class Student {
 
-    String studentNumber,firstName,lastName, email
+    String studentNumber,firstName,lastName, email, mobileNumber
     Date birthDate,enrollDate
-    static constraints = {
 
+    static constraints = {
+          studentNumber unique:true
+          email email:true
+          firstName blank:false
+          lastName blank:false
+          birthDate max: new Date()
     }
 }
